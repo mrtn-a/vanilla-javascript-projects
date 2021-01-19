9da9bad07e905eb3cafa61411b07a4dc1e471e2d
Martyna Markiel <martyna.markiel@gmail.com>
2021-01-19 22:23:59 +0100

    get elements from the DOM

---
+++ 1-form-validator/script.js
@@ -0,0 +1,5 @@
+const form = documentgetElementById('form');
+const username = documentgetElementById('username');
+const email = documentgetElementById('email');
+const password = documentgetElementById('password');
+const password2 = documentgetElementById('password2');
