bb3da97a6dae867b51e50a76fc3309c6c48d76a9
Martyna Markiel <martyna.markiel@gmail.com>
2021-01-20 20:14:25 +0100

    add JS function to toggle class on seats

--- 2-movie-seat-booking/script.js
+++ 2-movie-seat-booking/script.js
@@ -5,3 +5,11 @@ const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
 const ticketPrice = +movieSelect.value; // adding + will turn it into an integer (works as parseInt() )
+
+// Change class of one selected seat to .selected
+container.addEventListener('click', (e) => {
+  if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')
+    ) {
+    e.target.classList.toggle('selected');
+  }
+})
