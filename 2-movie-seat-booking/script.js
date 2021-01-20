457116bdf8871e2cfd9279619c88fd16a9c8dbcb
Martyna Markiel <martyna.markiel@gmail.com>
2021-01-20 22:33:07 +0100

    add JS function to update the tickets price and count

--- 2-movie-seat-booking/script.js
+++ 2-movie-seat-booking/script.js
@@ -4,12 +4,35 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
-const ticketPrice = +movieSelect.value; // adding + will turn it into an integer (works as parseInt() )
+let ticketPrice = +movieSelect.value; // adding + will turn it into an integer (works as parseInt() )
 
-// Change class of one selected seat to .selected
+// TODO
+// Add event listener on the container
+// Check if clicked element has a class of sear but not occupied seat
+// Add a class of selected then and call updateSelectedCount to make the calculations
+
+// Update total and count
+function updateSelectedCount() {
+  const selectedSeats = document.querySelectorAll('.row .seat.selected'); // putting seats into a Node list
+
+  const selectedSeatsCount = selectedSeats.length;
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+// Movie select event (change event)
+movieSelect.addEventListener('change', e => {
+  ticketPrice = +e.target.value;
+  updateSelectedCount();
+})
+
+
+// Seat click event - Change class of one selected seat to .selected
 container.addEventListener('click', (e) => {
   if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')
     ) {
     e.target.classList.toggle('selected');
+
+    updateSelectedCount();
   }
-})
+});
