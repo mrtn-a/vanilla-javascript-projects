b101b2101eaaab2c04b494c27499a4ce1cf049eb
Martyna Markiel <martyna.markiel@gmail.com>
2021-01-21 22:34:21 +0100

    prepare all selectors for JS functions

---
+++ 3-custom-video-player/script.js
@@ -0,0 +1,5 @@
+const video = document.getElementById('video');
+const play = document.getElementById('play');
+const stop = document.getElementById('stops');
+const progress = document.getElementById('progress');
+const timestamp = document.getElementById('timestamp');
