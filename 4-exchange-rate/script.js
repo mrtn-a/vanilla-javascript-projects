8e866f32def3631c27e0a10491ecae0211bafda7
Martyna Markiel <martyna.markiel@gmail.com>
2021-01-22 23:28:16 +0100

    select DOM elements needed for JS

---
+++ 4-exchange-rate/script.js
@@ -0,0 +1,7 @@
+const currencyEl_one = document.getEelementById("currency-one");
+const amountEl_one = document.getEelementById("amount-one");
+const currencyEl_two = document.getEelementById("currency-two");
+const amountEl_two = document.getEelementById("amount-two");
+
+const rateEl = document.getEelementById("rate");
+const swap = document.getEelementById("swap");
