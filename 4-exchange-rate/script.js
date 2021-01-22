4b991f4194cb6629d4cc7a0a99aca606aec05198
Martyna Markiel <martyna.markiel@gmail.com>
2021-01-22 23:44:08 +0100

    fetch rates and update DOM

--- 4-exchange-rate/script.js
+++ 4-exchange-rate/script.js
@@ -8,7 +8,19 @@ const swap = document.getElementById("swap");
 
 // Fetch exchange rates and update the DOM
 function calculate() {
-	console.log("ran");
+	const currency_one = currencyEl_one.value;
+	const currency_two = currencyEl_two.value;
+
+	fetch(`https://api.exchangerate-api.com/v4/latest/${currency_one}`)
+		.then((res) => res.json())
+		.then((data) => {
+			// console.log(data);
+			const rate = data.rates[currency_two]; // using as an index
+
+			rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
+
+			amountEl_two.value = (amountEl_one.value * rate).toFixed(2); // toFixed() > to set up how many decimals
+		});
 }
 
 // Event listeners
@@ -18,3 +30,10 @@ currencyEl_two.addEventListener("change", calculate);
 amountEl_two.addEventListener("input", calculate);
 
 calculate();
+
+swap.addEventListener("click", () => {
+	const temporaryValue = currencyEl_one.value;
+	currencyEl_one.value = currencyEl_two.value;
+	currencyEl_two.value = temporaryValue;
+	calculate();
+});
