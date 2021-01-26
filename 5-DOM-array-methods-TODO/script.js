9d908fe34e56a9157dfec0e393c88d90d4abfc13
Martyna Markiel <martyna.markiel@gmail.com>
2021-01-26 20:22:46 +0100

    calculate wealth w/ reduce()

--- 5-DOM-array-methods-TODO/script.js
+++ 5-DOM-array-methods-TODO/script.js
@@ -49,6 +49,19 @@ function showMillionaires() {
 
 	updateDOM();
 }
+
+// Calculate the total wealth
+function calculateWealth() {
+	const wealth = data.reduce((acc, user) => (acc += user.money), 0);
+
+	console.log(formatMoney(wealth));
+
+	const wealthEl = document.createElement("div");
+	wealthEl.innerHTML = `<h3>Total Wealth:
+	<strong>${formatMoney(wealth)}</strong></h3>`;
+
+	main.appendChild(wealthEl);
+}
 // Add new object to data arr
 function addData(obj) {
 	data.push(obj);
@@ -81,3 +94,4 @@ addUserBtn.addEventListener("click", getRandomUser);
 doubleBtn.addEventListener("click", doubleMoney);
 sortBtn.addEventListener("click", sortByRichest);
 showMillionairesBtn.addEventListener("click", showMillionaires);
+calculateWealthBtn.addEventListener("click", calculateWealth);
