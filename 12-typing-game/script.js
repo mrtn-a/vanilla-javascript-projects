eab3980832f2efbe54129ec6dc42b2c9aff60ad7
Martyna Markiel <martyna.markiel@gmail.com>
2021-01-30 23:13:43 +0100

    get random word and add it to the DOM functions

--- 12-typing-game/script.js
+++ 12-typing-game/script.js
@@ -7,3 +7,49 @@ const settingsBtn = document.getElementById("settings-btn");
 const settings = document.getElementById("settings");
 const settingsForm = document.getElementById("settings-form");
 const difficultySelect = document.getElementById("difficulty");
+
+// List of words for game
+const words = [
+	"sigh",
+	"tense",
+	"airplane",
+	"ball",
+	"pies",
+	"juice",
+	"warlike",
+	"bad",
+	"north",
+	"dependent",
+	"steer",
+	"silver",
+	"highfalutin",
+	"superficial",
+	"quince",
+	"eight",
+	"feeble",
+	"admit",
+	"drag",
+	"loving",
+];
+
+// Init word
+let randomWord;
+
+// Init score
+let score = 0;
+
+// Init time
+let time = 10;
+
+// Generate a random word from the array
+function getRandomWord() {
+	return words[Math.floor(Math.random() * words.length)];
+}
+
+// Add word to DOM
+function addWordToDOM() {
+	randomWord = getRandomWord();
+	word.innerHTML = randomWord;
+}
+
+addWordToDOM();
