de702a95fbd9fb4ef17b45231871c6480d514b43
Martyna Markiel <martyna.markiel@gmail.com>
2021-02-02 19:13:11 +0100

    select all DOM elements needed

---
+++ 9-expense-tracker/script.js
@@ -0,0 +1,7 @@
+const balance = document.getElementById("balance");
+const money_plus = document.getElementById("money_plus");
+const money_minus = document.getElementById("money_minus");
+const list = document.getElementById("list");
+const form = document.getElementById("form");
+const text = document.getElementById("text");
+const amount = document.getElementById("amount");
