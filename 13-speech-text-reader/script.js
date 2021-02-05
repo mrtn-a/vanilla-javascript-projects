fca84722abaf56e6e83a6b08a88cede60ee26b41
Martyna Markiel <martyna.markiel@gmail.com>
2021-02-05 20:26:19 +0100

    add all the DOM elements needed

---
+++ 13-speech-text-reader/script.js
@@ -0,0 +1,6 @@
+const main = document.querySelector("main");
+const voicesSelect = document.getElementById("voices");
+const textarea = document.getElementById("text");
+const readBtn = document.getElementById("read");
+const toggleBtn = document.getElementById("toggle");
+const closeBtn = document.getElementById("close");
